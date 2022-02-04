a4ff11374ef38c64dd36cf50112bfe58d0e8af8e
shahnawaz <khanshahnawaz1947@gmail.com>
2022-02-05 02:57:38 +0530

    init

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlz9U6ALisy5gZycpnpI98stz7Dlai7G8",
+    authDomain: "quiz-project-ebb40.firebaseapp.com",
+    projectId: "quiz-project-ebb40",
+    storageBucket: "quiz-project-ebb40.appspot.com",
+    messagingSenderId: "89166435872",
+    appId: "1:89166435872:web:90958cfe8f99797121ce45"
+  };
